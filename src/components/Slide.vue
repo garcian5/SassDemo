1171f9d5a26f122efd5cf144439d3b9cae12bbd6
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2023-04-13 16:32:25 -0600

    fleshed out slide component

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,15 +1,27 @@
-<script>
-  import { useSlideStore } from '@/stores/slide'
-  const slideStore = useSlideStore();
-  export default {
-    name: 'Slide'
-  }
+<script lang="ts">
+  export default { name: 'Slide' }
+</script>
 
+<script setup lang="ts">
+  const props = defineProps({
+    title: {
+      type: String,
+      default: 'New Title'
+    },
+    body: {
+      type: [String, HTMLElement],
+      default: 'New Body'
+    },
+    imgs: {
+      type: Array,
+      default: []
+    }
+  });
 </script>
 
 <template>
   <div>
-    <h1>{{ slideStore.title }}</h1>
+    <h1></h1>
     <!-- body will need to be embeded as an html element -->
   </div>
 </template>
--- src/stores/slide.ts
+++ src/stores/slide.ts
@@ -1,9 +1,5 @@
 import { defineStore } from 'pinia'
 
 export const useSlideStore = defineStore('slide', {
-  state: () => ({
-    title: 'New Title' as string,
-    body: 'New Body' as string | HTMLElement,
-    imgs: [] as string[]
-  }),
+  state: () => ({}),
 })
