8503dc7436811f34f7d879fd272b434e8ad553e7
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2023-04-13 16:55:55 -0600

    added slides component

---
+++ src/components/Slides.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Slide from './Slide.vue'
+  export default {
+    components: { Slide },
+    name: 'Slides'
+  }
+</script>
+
+<template>
+  <div>
+    <slide></slide>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
