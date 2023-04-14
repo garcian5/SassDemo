4b742580c8f9a674c5171aaf36cb8f9a21e25d18
Nheljee Garcia <ngarcia@local.sequiter.com>
2023-04-14 09:14:09 -0600

    added cert plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'SassDemo',
-  plugins: [vue()],
+  server: { https: true },
+  plugins: [mkcert(), vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
