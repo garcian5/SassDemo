0fa6c5d2ad26b3998053d69b2ae60648a7091a2a
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2023-04-13 22:29:18 -0600

    created a sample data on the switchfilter

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -3,7 +3,7 @@
 </script>
 
 <script setup lang="ts">
-  import { onMounted, reactive } from 'vue';
+  import { computed, onMounted, reactive, ref } from 'vue';
   import { SlideData } from '../data/schema/SlideData';
 
   const props = defineProps({
@@ -19,6 +19,8 @@
     bodyList: []
   });
 
+  const clickedDiv = ref<number | 0>(0);;
+
   //#region methods
   const setSlideData = () => {
     if (props.slide) {
@@ -31,6 +33,11 @@
     }
   }
   //#endregion
+
+  const selectDiv = (num: number) => {
+    clickedDiv.value = num;
+    console.warn("hello", clickedDiv)
+  }
 
   onMounted(setSlideData);
   defineExpose({setSlideData});
@@ -61,6 +68,12 @@
       <img v-for="(img, index) in slide.imgs" :src="img" alt="" :key="img + index" />
     </div>
   </div>
+
+  <div @click="selectDiv(1)" :class="{'switchFilter': clickedDiv == 1}">1</div>
+  <div @click="selectDiv(2)" :class="{'switchFilter': clickedDiv == 2}">2</div>
+
+  <h3 class="hover">Hover Me</h3>
+  <img src="https://picsum.photos/id/1022/200/200" alt="a nighty sky">
 </template>
 
 <style lang="scss" scoped>
@@ -88,5 +101,22 @@
       margin: 10px auto;
     }
   }
+}
+
+
+/* https://freefrontend.com/css-border-animations/ */
+/* https://codepen.io/t_afif/pen/rNJegrz */
+.switchFilter {
+  border: 8px solid;
+  border-image: repeating-linear-gradient(135deg,#118617 0 10px,#118617 0 20px,#118617 0 30px) 2;
+  -webkit-mask: 
+    conic-gradient(from 180deg at top 4px right 4px, #0000 90deg,#000 0)
+     var(--animate,200%) 0  /200% var(--animate,4px) border-box no-repeat,
+    conic-gradient(at bottom 4px left  4px,  #0000 90deg,#000 0)
+     0   var(--animate,200%)/var(--animate,4px) 200% border-box no-repeat,
+    linear-gradient(#000 0 0) padding-box no-repeat;
+  transition: .5s, -webkit-mask-position .5s .5s;
+  --animate: 100%;
+  transition: .5s, -webkit-mask-size .5s .5s;
 }
 </style>
